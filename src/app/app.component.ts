52279d31318a75ca012167bfe22d072c30b3ebeb
Gillan Flaherty <flahertygillian@gmail.com>
2024-09-05 10:52:15 -0500

    added reusable dashboard-item component that wraps each dashboard item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
-import { ServerStatusComponent } from "./dashbard/server-status/server-status.component";
-import { TicketsComponent } from "./dashbard/tickets/tickets.component";
-import { TrafficComponent } from "./dashbard/traffic/traffic.component";
+import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
+import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TicketsComponent, TrafficComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TicketsComponent, TrafficComponent, DashboardItemComponent],
 })
 export class AppComponent {
   
